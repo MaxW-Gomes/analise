d8506af146954f17263030848c3f629f291b996d
MaxW-Gomes <75535730+MaxW-Gomes@users.noreply.github.com>
2020-12-05 13:59:04 -0300

    Create plugins.sh

---
+++ plugins.sh
@@ -0,0 +1,13 @@
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar  wp;
+
+./wp plugin install autoptimize --activate;
+
+./wp plugin install "Disable XML-RPC Pingback" --activate;
+
+./wp plugin install "Heartbeat Control" --activate;
+
+./wp plugin install "rvg-optimize-database" --activate;
+
+./wp plugin install imsanity --activate;
+
+./wp plugin install "LiteSpeed Cache --activate"
