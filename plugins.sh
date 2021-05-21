fccb505c8738eee88bc03ca6e62a48b61626818f
Max Gomes <maxwilton1994@gmail.com>
2021-05-21 16:20:12 -0300

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -1,14 +1,10 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar  wp;
 
-./wp plugin install autoptimize --activate;
-
 ./wp plugin install "disable-xml-rpc-pingback" --activate;
 
 ./wp plugin install "heartbeat-control" --activate;
 
 ./wp plugin install "rvg-optimize-database" --activate;
-
-./wp plugin install imsanity --activate;
 
 ./wp plugin install "litespeed-cache";
 rm wp
