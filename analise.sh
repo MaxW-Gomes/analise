d47beb2237063a7d09176ca94d60c9cf24c48c7a
Max Gomes <maxwilton1994@gmail.com>
2021-05-21 23:53:57 -0300

    Alterações

--- analise.sh
+++ analise.sh
@@ -15,7 +15,7 @@ awk -F\" '{print $2}' $log | awk '{print $2}' | sort | uniq -c | sort -nr | head
 
 echo -e "\n\n 10 URLs mais acessadas, ignorando query strings: \n";
 
-awk -F\" '{print $4}' $log | awk -F'?' {'print $1'} | sort | uniq -c | sort -nr | head;
+awk -F\" '{print $2}' $log | awk -F'?' {'print $1'} | sort | uniq -c | sort -nr | head;
 
 echo -e "\n\n 10 user-agents que mais acessaram o website: \n";
 
@@ -31,6 +31,4 @@ awk '{print $9}' $log | sort | uniq -c | sort -nr | head;
 
 echo -e "\n\n Somando o tráfego:\n";
 
-awk '{sum+=$10} END {print sum/1024/1024/1024 " GB"}' log;
-
-
+awk '{sum+=$10} END {print sum/1024/1024/1024 " GB"}' ${log};
